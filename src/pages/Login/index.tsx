0a631833ea53e4b5b8f5e54a2a40cf140b7e9ee9
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-08-25 19:22:55 -0300

    Criando formulário de login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import { Link } from "react-router";
+import { InputCustom } from "../../components/UI/InputCustom";
+import { ButtonCustom } from "../../components/UI/ButtonCustom";
 
 export default function Login() {
   return (
-    <div>
-      <h1>Página Login</h1>
-    </div>
+    <main className="w-full h-dvh flex items-center">
+      <div className="container max-w-[800px] mx-auto">
+        <Link to={"/login"}>
+          <img
+            src="/public/webCarrosLogo.svg"
+            alt="web Carros Logo"
+            className="w-60 md:w-96 mx-auto mb-14"
+          />
+        </Link>
+
+        <form className="space-y-6">
+          <InputCustom type="email" placeholder="Digite seu E-mail" />
+          <InputCustom type="password" placeholder="Digite sua Senha" />
+          <ButtonCustom className="w-full">Acessar</ButtonCustom>
+        </form>
+      </div>
+    </main>
   );
 }
