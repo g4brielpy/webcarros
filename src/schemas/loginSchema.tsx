c6e95023e7ba273b641a4054dcbf8a62e1323352
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-08-27 07:17:56 -0300

    Criando schema do formulário

---
+++ src/schemas/loginSchema.tsx
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const loginSchema = z.object({
+  email: z.email("Insira um email válido").nonempty("O email é obrigatório"),
+  password: z
+    .string()
+    .nonempty("A senha é obrigatório")
+    .min(4, "Insira uma senha com mais de 4 dígitos"),
+});
+
+export type FormData = z.infer<typeof loginSchema>;
