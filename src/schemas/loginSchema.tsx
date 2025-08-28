06bc73ccb9f9146aa28569f43bb5b2b72aa8b6f1
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-08-28 07:08:10 -0300

    Alterando tipe do email

--- src/schemas/loginSchema.tsx
+++ src/schemas/loginSchema.tsx
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  email: z.string("Insira um email válido").nonempty("O email é obrigatório"),
+  email: z.email("Insira um email válido").nonempty("O email é obrigatório"),
   password: z
     .string()
     .nonempty("A senha é obrigatório")
