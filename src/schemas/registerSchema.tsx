c0a5370a3977eb2419466ef12c47c3df27120599
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-08-28 07:12:47 -0300

    Definindo schema da página register

---
+++ src/schemas/registerSchema.tsx
@@ -0,0 +1,12 @@
+import z from "zod";
+
+export const registerSchema = z.object({
+  name: z.string().nonempty("O nome é obrigatório"),
+  email: z.email("Insira um email válido").nonempty("O email é obrigatório"),
+  password: z
+    .string()
+    .nonempty("A senha é obrigatório")
+    .min(4, "Insira uma senha com mais de 4 dígitos"),
+});
+
+export type FormData = z.infer<typeof registerSchema>;
