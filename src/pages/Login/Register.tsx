a66f591dee688af4111a988911a76feae5a9f9b1
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-08-28 07:21:25 -0300

    Adicionando validação de schema na página register

--- src/pages/Login/Register.tsx
+++ src/pages/Login/Register.tsx
@@ -2,11 +2,23 @@ import { Link } from "react-router";
 import { InputCustom } from "../../components/UI/InputCustom";
 import { ButtonCustom } from "../../components/UI/ButtonCustom";
 
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { registerSchema, FormData } from "../../schemas/registerSchema";
+
 export default function Register() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(registerSchema) });
+
+  const onSubmitRegister = (data: FormData) => console.log(data);
+
   return (
     <main className="w-full h-dvh flex items-center">
       <div className="container px-4 max-w-[800px] mx-auto">
-        <Link to={"/login"}>
+        <Link to={"/register"}>
           <img
             src="/public/webCarrosLogo.svg"
             alt="web Carros Logo"
@@ -14,10 +26,25 @@ export default function Register() {
           />
         </Link>
 
-        <form className="space-y-6">
-          <InputCustom type="text" placeholder="Digite seu nome completo" />
-          <InputCustom type="email" placeholder="Digite seu E-mail" />
-          <InputCustom type="password" placeholder="Digite sua Senha" />
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmitRegister)}>
+          <InputCustom
+            type="text"
+            placeholder="Digite seu nome completo"
+            {...register("name")}
+            error={errors.name?.message}
+          />
+          <InputCustom
+            type="email"
+            placeholder="Digite seu E-mail"
+            {...register("email")}
+            error={errors.email?.message}
+          />
+          <InputCustom
+            type="password"
+            placeholder="Digite sua Senha"
+            {...register("password")}
+            error={errors.password?.message}
+          />
           <ButtonCustom className="w-full">Acessar</ButtonCustom>
         </form>
 
