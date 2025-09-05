bf5c33d99e67a19af7e51d0f6f0cd5e6720a2cdd
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-09-05 07:54:57 -0300

    Criando funções de login e logout

--- src/contexts/AuthProvider.tsx
+++ src/contexts/AuthProvider.tsx
@@ -1,11 +1,32 @@
-import { onAuthStateChanged } from "firebase/auth";
 import { ReactNode, useEffect, useState } from "react";
-import { auth } from "../firebase/firebaseConnection";
 import { AuthContext, userProps } from "./AuthContext";
+
+import { auth } from "../firebase/firebaseConnection";
+import {
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<userProps>(null);
   const signed: boolean = !!user;
+
+  function login(email: string, password: string) {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(
+          "Usuário logado com sucesso: " + userCredential.user.displayName
+        );
+      })
+      .catch((error) => {
+        console.log("Erro ao fazer login:", error.message);
+      });
+  }
+
+  function logout() {
+    signOut(auth);
+  }
 
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, (userState) => {
