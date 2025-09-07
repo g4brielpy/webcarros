5b5b2f31fbf74c451395ea21c25345a900d66796
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-09-07 18:05:01 -0300

    Validando icones

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,12 @@
+import { useContext } from "react";
+import { AuthContext, AuthContextType } from "../../contexts/AuthContext";
+
 import { Link } from "react-router";
 
 import { FiUser, FiLogIn } from "react-icons/fi";
 
 export default function Header() {
-  const signed = false;
+  const authContext = useContext<AuthContextType | null>(AuthContext);
 
   return (
     <header className="h-16 px-4 flex items-center justify-center shadow-md">
@@ -12,8 +15,8 @@ export default function Header() {
           <img src="/public/webCarrosLogo.svg" alt="web Carros Logo" />
         </Link>
 
-        {signed ? (
-          <Link to={"/dashboard"}>
+        {authContext?.signed ? (
+          <Link to={"/dashboard/newcar"}>
             <FiUser size={24} />
           </Link>
         ) : (
