c9aa07fb653729b8654cc7f4c381e2900ddd6c8a
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-09-07 17:58:31 -0300

    hook para deslogar usuário

---
+++ src/hooks/useLogout.tsx
@@ -0,0 +1,12 @@
+// src/hooks/useLogoutOnMount.ts
+import { useEffect, useContext } from "react";
+import { AuthContext, AuthContextType } from "../contexts/AuthContext";
+
+export function useLogoutOnMount() {
+  const auth = useContext<AuthContextType | null>(AuthContext);
+
+  useEffect(() => {
+    auth?.logout();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+}
