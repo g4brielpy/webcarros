d10d335ce899276d5aa66ba3807b6e5a1a6d7aed
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-09-08 21:29:30 -0300

    Adicionando card dos meus carros

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,16 @@
 import { HeaderDashboard } from "../../components/HeaderDashboard";
+import { CardCar } from "../../components/CardCar";
 
 export default function Dashboard() {
   return (
-    <div>
+    <div className="container px-4 mx-auto my-8">
       <HeaderDashboard />
-      <h1>Página Dashboard</h1>
+      <main className="mt-8">
+        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <CardCar />
+          <CardCar />
+        </section>
+      </main>
     </div>
   );
 }
