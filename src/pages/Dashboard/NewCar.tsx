c6b8cbfb0b8e1536b85130b4596dc6a58403a4e9
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-09-15 07:30:59 -0300

    Criando formulário de cadastro de carro

--- src/pages/Dashboard/NewCar.tsx
+++ src/pages/Dashboard/NewCar.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import { FiUpload } from "react-icons/fi";
+import { InputCustom } from "../../components/UI/InputCustom";
 
 export default function NewCar() {
   return (
-    <div>
-      <h1>Página dashboard - novo carro</h1>
+    <div className="container px-4 mx-auto my-8">
+      <main>
+        <section>
+          <button>
+            <FiUpload />
+          </button>
+        </section>
+
+        <section>
+          <form className="space-y-6">
+            <InputCustom type="text" label="Nome do Carro" />
+            <InputCustom type="text" label="Modelo" />
+            <div>
+              <InputCustom label="Ano" />
+              <InputCustom label="Km's rodados" />
+            </div>
+            <InputCustom label="Valor em R$" />
+            <InputCustom type="text" label="Cidade" />
+            <InputCustom type="text" label="WhatsApp" />
+            <InputCustom type="text" label="Descrição" />
+          </form>
+        </section>
+      </main>
     </div>
   );
 }
