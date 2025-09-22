e66b9f96599032ed6d2f8fbe7e80afa690e72142
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-09-22 07:29:49 -0300

    feat: adiciona configuração e conexão com Firebase (Firestore, Auth e Storage)

--- src/firebase/firebaseConnection.ts
+++ src/firebase/firebaseConnection.ts
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -22,5 +23,6 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+export const storage = getStorage(app);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
