092cfe415ff4aff4bfc1dd24cd67bb71c378f987
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-09-26 07:52:04 -0300

    feat(utils): Alteração das mensagens de erros

--- src/utils/getAllImagesUrls.tsx
+++ src/utils/getAllImagesUrls.tsx
@@ -26,15 +26,13 @@ export async function getAllImagesUrls(userId: string) {
         case "storage/object-not-found":
           return []; // Pasta vazia
         case "storage/unauthorized":
-          throw new Error(
-            "Voc\u00ea n\u00e3o tem permiss\u00e3o para listar essas imagens."
-          );
+          throw new Error("Você não tem permissão para listar essas imagens.");
         case "storage/unknown":
           throw new Error(
             "Erro desconhecido ao listar imagens no Firebase Storage."
           );
         default:
-          throw new Error("Erro ao listar imagens. C\u00f3digo: " + error.code);
+          throw new Error("Erro ao listar imagens. Código: " + error.code);
       }
     }
 
