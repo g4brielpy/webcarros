a0e888341ca7f7479f91a239294c41d4cb89b918
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-09-28 15:52:03 -0300

    feat: adicionar biblioteca de notificações Sonner

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from "react-dom/client";
 
 import "./index.css";
 import App from "./App.tsx";
+import { Toaster } from "sonner";
 
 import { AuthProvider } from "./contexts/AuthProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
+      <Toaster position="top-right" richColors closeButton expand />
       <App />
     </AuthProvider>
   </StrictMode>
